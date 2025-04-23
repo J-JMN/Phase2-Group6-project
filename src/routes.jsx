d75d7bb2ca74818c96185bac201205dcdb1ec6a1
dennissammy77 <dennissammy77@gmail.com>
2025-04-23 20:22:43 +0300

    feat: pages set up

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,25 @@
 import App from "./App";
+import Inventory from "./pages/Inventory";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import ShoppingList from "./pages/ShoppingList";
 
 const routes = [
     {
         path: "/",
-        element: <App />
+        element: <Login />
+    },
+    {
+        path: "/home",
+        element: <ShoppingList />
+    },
+    {
+        path: "/inventory",
+        element: <Inventory />
+    },
+    {
+        path: "/settings",
+        element: <Settings />
     },
 ];
 
