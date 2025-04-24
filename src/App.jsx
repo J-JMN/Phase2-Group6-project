6fdfc752d1baefb65f3658f2a37b6e02d60ec92a
Dominic Kipkorir <harsh979pal@gamil.com>
2025-04-24 20:30:13 +0300

    Added ShoppingList code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import routes from './routes';
 
 function App() {
   return (
-    <div>
-      <h1>ShopMate</h1>
-    </div>
-  )
+    <Router>
+      <Routes>
+        {routes.map((r, index) => (
+          <Route key={index} path={r.path} element={r.element} />
+        ))}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
