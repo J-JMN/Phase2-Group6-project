260750b5ce0147581c06d8e7c651a9055a1e1c61
Pauline Moraa <pmoraa431@gmail.com>
2025-04-25 12:55:11 +0300

    made changes in routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import SettingsPage from "./pages/Settings";
 import ShoppingList from "./pages/ShoppingList";
 import Register from "./pages/Register";
+import SignUp from "./pages/SignUp";
 
 const routes = [
   {
@@ -11,7 +12,7 @@ const routes = [
     element: <App />, // App is now the layout
     children: [
       { path: "login", element: <Login /> },
-      { path: "Register", element: <Register /> },
+      { path: "SignUp", element: < SignUp />},
       { path: "home", element: <ShoppingList /> },
       { path: "inventory", element: <Inventory /> },
       { path: "settings", element: <SettingsPage /> },
