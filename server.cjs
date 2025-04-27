d0428415a639198470018f57c08b30e793a54eee
dennissammy77 <dennissammy77@gmail.com>
2025-04-27 16:13:18 +0300

    path fix  for json call

--- server.cjs
+++ server.cjs
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
